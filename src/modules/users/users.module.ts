51ead603edf2d1aa0aed91b05d6cdea8f2ab45f2
Mark-goga <markgala06@gmail.com>
2025-05-06 21:26:17 +0300

    add architecture

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,6 +3,22 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: AUTH_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: AUTH_PACKAGE_NAME,
+          protoPath: join(
+            process.cwd(),
+            'dist',
+            `${PROTO_PATH}/user/user.proto`,
+          ),
+        },
+      },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
